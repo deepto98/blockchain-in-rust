4e14b04961a59fc867faa495ec5e056dff068ee4
deepto <ddepp98@outlook.com>
2023-04-17 12:06:35 +0530

    added implementation for new block

--- src/block.rs
+++ src/block.rs
@@ -8,5 +8,27 @@ pub struct Block {
 }
 
 pub struct Blockchain {
-    blocks: Vec<Block>
+    blocks: Vec<Block>,
+}
+
+impl Block {
+    pub fn new_block(data: String, prev_block_hash: String, height: usize) -> Result<Block> {
+        let timestamp: u128 = SystemTime::now()
+            .duration_since(SystemTime::UNIX_EPOCH)?
+            .as_millis();
+        let mut Block = Block {
+            timestamp: timestamp,
+            transactions: data,
+            prev_block_hash,
+            hash: String::new(),
+            height,
+            nonce: 0,
+        };
+        block.run_proof_of_work()?;
+        Ok(block)
+    }
+
+    fn run_proof_of_work(&mut self) -> Result<()> {
+        
+    }
 }
