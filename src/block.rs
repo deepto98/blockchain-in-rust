cc15062c8227b51d4db021d360b79ca017b66dc0
deepto <ddepp98@outlook.com>
2023-04-17 12:42:04 +0530

    Implemented run_proof_of_work,prepare_hash_data and validate functions

--- src/block.rs
+++ src/block.rs
@@ -1,3 +1,8 @@
+use std::time::SystemTime;
+use crypto::{digest::Digest, sha2::Sha256};
+use log::info;
+
+const TARGET_HEXT: usize = 4;
 pub struct Block {
     timestamp: u128,
     transactions: String,
@@ -10,6 +15,8 @@ pub struct Block {
 pub struct Blockchain {
     blocks: Vec<Block>,
 }
+
+pub type Result<T> = std::result::Result<T,failure::Error>;
 
 impl Block {
     pub fn new_block(data: String, prev_block_hash: String, height: usize) -> Result<Block> {
@@ -29,6 +36,35 @@ impl Block {
     }
 
     fn run_proof_of_work(&mut self) -> Result<()> {
-        
+        info!("Mining the block");
+        while !self.validate()? {
+            self.nonce += 1;
+        }
+        let data = self.prepare_hash_data()?;
+        let mut hasher = Sha256::new();
+        hasher.input(&data[..]);
+        self.hash=hasher.result_str();
+        Ok(())
+    }
+    fn prepare_hash_data(&self) -> Result<Vec<u8>> {
+        let content=(
+            self.prepare_hash_data().clone(),
+            self.transactions.clone(),
+            self.timestamp,
+            TARGET_HEXT,
+            self.nonce
+        );
+        let bytes:Vec<u8> =bincode::serialize(&content)?;
+        Ok(bytes)
+    }
+
+    fn validate(self) -> Result<bool> {
+        let data: Vec<u8> = self.prepare_hash_data()?;
+        let mut hasher = Sha256::new();
+        hasher.input(&data[..]);
+        let mut vec1: Vec<u8> = Vec::new();
+        vec1.resize(TARGET_HEXT, '0' as u8);
+        println!("{:?}",vec1);
+        Ok(&hasher.result_str()[0..TARGET_HEXT] == String::from_utf8(vec1))
     }
 }
